427e29dfd20a192fdd88394f14962749b567d0ad
Alexander Chepel <chepel.alexander@gmail.com>
2020-07-31 21:56:20 +0300

    Выполнено задание 3.1 - вывод с Toast информации о жизненном цикле обеих Activity.

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +25,43 @@ public class MainActivity extends AppCompatActivity {
         findViewsById();
         setSettingsActivity();
         setCurrentWeather();
+        Toast.makeText(getApplicationContext(), "MainActivity.onCreate()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "MainActivity.onStart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "MainActivity.onStop()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), "MainActivity.onRestart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected  void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "MainActivity.onPause()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "MainActivity.onResume()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), "MainActivity.onDestroy()", Toast.LENGTH_SHORT).show();
     }
 
     private void setSettingsActivity() {
--- app/src/main/java/com/example/myapp/SettingsActivity.java
+++ app/src/main/java/com/example/myapp/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +19,44 @@ public class SettingsActivity extends AppCompatActivity {
         setTitle(R.string.app_name_settings);
         findViewsById();
         setSpnWeatherPoint();
+        Toast.makeText(getApplicationContext(), "SettingsActivity.onCreate()", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "SettingsActivity.onStart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "SettingsActivity.onStop()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), "SettingsActivity.onRestart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected  void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "SettingsActivity.onPause()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "SettingsActivity.onResume()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), "SettingsActivity.onDestroy()", Toast.LENGTH_SHORT).show();
     }
 
     private void findViewsById() {
